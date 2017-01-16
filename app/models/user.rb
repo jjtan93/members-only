9b354c6d9ec30ac8d320b4b338ac3c9b3e4f0076
Jian Jing Tan <kendricktjj93@yahoo.com>
2017-01-16 17:56:29 +0800

    Add a remember_digest to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
+  before_create :create_remember_digest
   
   has_secure_password
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.encrypt(string)
+    Digest::SHA1.hexdigest(string.to_s)
+  end
+  
+  private
+    # Creates and assigns the remember token and digest.
+    def create_remember_digest
+      self.remember_token  = User.new_token
+      self.remember_digest = User.encrypt(remember_token)
+    end
+    
 end
